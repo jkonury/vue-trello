d8e0a3bba6c3f131060ab625691f7d3eb835866a
jkonury <jkonury32@gmail.com>
2018-12-24 17:54:50 +0900

    router view

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
+import Home from '../component/Home.vue'
+import Login from '../component/Login.vue'
+import NotFound from '../component/NotFound.vue'
 
 Vue.use(VueRouter)
-
-const Login = { template: '<div>Login Page</div>' }
-const NotFound = { template: '<div>Page not found</div>' }
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: App },
+    { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '*', component: NotFound }
   ]
