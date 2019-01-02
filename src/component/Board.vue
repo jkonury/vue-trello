ffa13f872e5d32d74cfc2cd52b53c1dc5a79632e
jkonury <jkonury32@gmail.com>
2019-01-02 22:11:44 +0900

    card - move refactoring

--- src/component/Board.vue
+++ src/component/Board.vue
@@ -23,8 +23,7 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List'
-import dragula from 'dragula'
-import 'dragula/dist/dragula.css'
+import dragger from '../utils/dragger'
 
 export default {
   components: {
@@ -47,54 +46,7 @@ export default {
     this.board_id = this.$route.params.board_id
   },
   updated() {
-    if (this.dragulaCards) {
-      this.dragulaCards.destroy()
-    }
-
-    this.dragulaCards = dragula([
-      ...Array.from(this.$el.querySelectorAll('.card-list'))
-    ]).on('drop', (el, wrapper, target, siblings) => {
-      const targetCard = {
-        id: el.dataset.cardId * 1,
-        pos: 65535
-      }
-
-      let prevCard = null
-      let nextCard = null
-
-      Array.from(wrapper.querySelectorAll('.card-item'))
-        .forEach((el, index, arr) => {
-          const cardId = el.dataset.cardId * 1
-          if (cardId == targetCard.id) {
-            if (index > 0 ) {
-              prevCard = {
-                id: arr[index - 1].dataset.cardId * 1,
-                pos: arr[index - 1].dataset.cardPos * 1
-              }
-            } else {
-              prevCard = null
-            }
-
-            if (index < arr.length - 1) {
-              nextCard = {
-                id: arr[index + 1].dataset.cardId * 1,
-                pos: arr[index + 1].dataset.cardPos * 1
-              }
-            } else {
-              nextCard = null
-            }
-          }
-        })
-
-      if (!prevCard && nextCard) {
-        targetCard.pos = nextCard.pos / 2
-      } else if (!nextCard && prevCard) {
-        targetCard.pos = prevCard.pos * 2
-      } else if (prevCard && nextCard) {
-        targetCard.pos = (prevCard.pos + nextCard.pos) / 2
-      }
-      this.UPDATE_CARD(targetCard)
-    })
+    this.setCardDragabble()
   },
   methods: {
     ...mapActions([
@@ -105,6 +57,37 @@ export default {
       this.loading = true
       this.FETCH_BOARD({id: this.$route.params.board_id})
           .then(() => this.loading = false)
+    },
+    setCardDragabble() {
+      if (this.dragulaCards) {
+        this.dragulaCards.destroy()
+      }
+
+      this.dragulaCards = dragger.init(Array.from(this.$el.querySelectorAll('.card-list')))
+
+      this.dragulaCards.on('drop', (el, wrapper, target, siblings) => {
+        const targetCard = {
+          id: el.dataset.cardId * 1,
+          pos: 65535
+        }
+
+        const {prev, next} = dragger.sibling({
+          el,
+          wrapper,
+          candidates: Array.from(wrapper.querySelectorAll('.card-item')),
+          type: 'card'
+        })
+
+        if (!prev && next) {
+          targetCard.pos = next.pos / 2
+        } else if(!next && prev) {
+          targetCard.pos = prev.pos * 2
+        } else if(prev && next) {
+          targetCard.pos = (prev.pos + next.pos) / 2
+        }
+
+        this.UPDATE_CARD(targetCard)
+      })
     }
   }
 }
---
+++ src/utils/dragger.js
@@ -0,0 +1,40 @@
+import dragula from 'dragula'
+import 'dragula/dist/dragula.css'
+
+const dragger = {
+  init(container) {
+    return dragula([...container])
+  },
+  sibling({el, wrapper, candidates, type}) {
+    const curId = el.dataset[type + 'Id'] * 1
+    let prev = null
+    let next = null
+
+    candidates.forEach((el, index, arr) => {
+      const id = el.dataset[type + 'Id'] * 1
+      if (id == curId) {
+        if (index > 0 ) {
+          prev = {
+            id: arr[index - 1].dataset[type + 'Id'] * 1,
+            pos: arr[index - 1].dataset[type + 'Pos'] * 1
+          }
+        } else {
+          prev = null
+        }
+
+        if (index < arr.length - 1) {
+          next = {
+            id: arr[index + 1].dataset[type + 'Id'] * 1,
+            pos: arr[index + 1].dataset[type + 'Pos'] * 1
+          }
+        } else {
+          next = null
+        }
+      }
+    })
+
+    return {prev, next}
+  }
+}
+
+export default dragger
